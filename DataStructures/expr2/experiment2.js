52d38d10203eb9ee0f31f9ce09626cf3d6de1770
Dharmeet Singh <dharmeet@vlabs.ac.in>
2014-03-14 16:15:00 +0530

    Updated experiment2.js

--- DataStructures/expr2/experiment2.js
+++ DataStructures/expr2/experiment2.js
@@ -26,27 +26,23 @@ window.model = {
             this.mExpr = this.mExpr.substring(1);
             mTermValue = mTermValue + this.evalExpr();
             if (this.mExpr.length > 0) {
-        		if (this.mExpr.charAt(0) == this.mSub) {
-        			this.mExpr = this.mExpr.substring(1);
-        			return mTermValue - this.evalExpr();
-        		}
-        		else if (this.mExpr.charAt(0) == this.mAdd) {
-        			this.mExpr = this.mExpr.substring(1);
-        			return mTermValue + this.evalExpr();
-        		} else if (this.mExpr.charAt(0) != this.mClBrckt) 
-        		    throw new IOException(this.mExpr); 
+        	    if (this.mExpr.charAt(0) == this.mSub) {
+                    this.mExpr = this.mExpr.substring(1);
+                    return mTermValue - this.evalExpr();
+                }
+                else if (this.mExpr.charAt(0) == this.mAdd) {
+                    this.mExpr = this.mExpr.substring(1);
+                    return mTermValue + this.evalExpr();
+                } else if (this.mExpr.charAt(0) != this.mClBrckt) 
+                    throw new IOException(this.mExpr); 
             } 
-            else 
-        	    return mTermValue; 
+           // else 
+             //   return mTermValue; 
         } 
         else if (this.mExpr.length > 0 && this.mExpr.charAt(0) == this.mSub) {
-            
             this.mExpr = this.mExpr.substring(1);
-  
             mTermValue = mTermValue + (-1)*this.evalTerm();
-
             if (this.mExpr.length > 0) { // for expression like a*b - c + d*e
-
                 if (this.mExpr.charAt(0) == this.mSub) {
                     this.mExpr = this.mExpr.substring(1);
                     return mTermValue - this.evalExpr();
@@ -64,11 +60,11 @@ window.model = {
 
     evalTerm: function() {
         console.log("Entering evalTerm");
-	    var mFactor = this.evalFactor();
+        var mFactor = this.evalFactor();
 
-	    if (this.mExpr.length > 0) {
-		    if (this.mExpr.charAt(0) == this.mMul) {
-				this.mExpr = this.mExpr.substring(1);
+        if (this.mExpr.length > 0) {
+           if (this.mExpr.charAt(0) == this.mMul) {
+                this.mExpr = this.mExpr.substring(1);
                 mFactor = mFactor * this.evalTerm();
                 return this.evalHPTerm(mFactor);
             } 
@@ -86,7 +82,7 @@ window.model = {
                     throw new IOException();
                 mFactor = mFactor % mDivisor;
                 return this.evalHPTerm(mFactor);
-            }
+            } 
             else if (this.mExpr.charAt(0) == this.mExp) {
                 this.mExpr = this.mExpr.substring(1);
                 // for expression of type a^b*c we should first calculate a^b
@@ -103,17 +99,13 @@ window.model = {
         var mNumber;
         var numb = "";
         var len = 0;
-    	if (this.mExpr.length > 0) {
+        if (this.mExpr.length > 0) {
             if ((this.mExpr.charAt(0) >= this.mZero && this.mExpr.charAt(0) <= this.mNine) || this.mExpr.charAt(0) == this.mDot) {
                 numb = this.mExpr.match("[0-9]*\\.?[0-9]+");
-                
                 console.log(numb);
                 mNumber = parseFloat(numb);
                 console.log(mNumber);
-                //}
-
-                //while(this.mExpr[len]==numb[len])
-                  //  len = len + 1;
+                
                 if (this.mExpr.length == numb[0].length) {
                     console.log("mExpr equals numb length");
                     this.mExpr = "";
@@ -122,7 +114,6 @@ window.model = {
                     console.log(numb[0].length);
                     this.mExpr = this.mExpr.substring(numb[0].length);
                     console.log(this.mExpr);
-
                 }
                 console.log(mNumber);
                 return mNumber;
@@ -147,7 +138,7 @@ window.model = {
     
     evalHPTerm: function(mFactor) {
         console.log("Entering evalHPTerm");
-    	if (this.mExpr.length > 0) {
+        if (this.mExpr.length > 0) {
             if (this.mExpr.charAt(0) == this.mAdd) {
                 this.mExpr = this.mExpr.substring(1);
                 return mFactor + this.evalExpr();
@@ -202,56 +193,56 @@ window.model = {
 
 window.view = {
 
-	addClickEvent: function (id, method) {
-		//console.log('clicked');
-		//console.log(id);
-		var element = document.getElementById(id);
-		element.addEventListener('click', method, false);
-	},
+    addClickEvent: function (id, method) {
+        //console.log('clicked');
+        //console.log(id);
+        var element = document.getElementById(id);
+        element.addEventListener('click', method, false);
+    },
 
-	activateEvents: function () {
-		this.addClickEvent('keyOpenBrcktId', function() { view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyCloseBrcktId', function() { view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keySevenId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyEightId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyNineId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyDivId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyExpId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyFourId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyFiveId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keySixId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyMulId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyModId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyOneId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyTwoId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyThreeId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keySubId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyOneByXId', function() {view.setInnerHTMLOneByX('screenId') });
-		this.addClickEvent('keyZeroId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyUnaryOpId', function() {view.setInnerHTMLUnary('screenId') });
-		this.addClickEvent('keyDotId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyAddId', function() {view.setInnerHTML('screenId', this.value) });
-		this.addClickEvent('keyEqualToId', function() {view.setFinalExpression('screenId') });
-		this.addClickEvent('keyClearId', function() {view.clearScreen('screenId') });
-		this.addClickEvent('keyBackspaceId', function() {view.setInnerHTMLBackspace('screenId') });
-	},
+    activateEvents: function () {
+        this.addClickEvent('keyOpenBrcktId', function() { view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyCloseBrcktId', function() { view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keySevenId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyEightId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyNineId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyDivId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyExpId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyFourId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyFiveId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keySixId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyMulId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyModId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyOneId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyTwoId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyThreeId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keySubId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyOneByXId', function() {view.setInnerHTMLOneByX('screenId') });
+        this.addClickEvent('keyZeroId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyUnaryOpId', function() {view.setInnerHTMLUnary('screenId') });
+        this.addClickEvent('keyDotId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyAddId', function() {view.setInnerHTML('screenId', this.value) });
+        this.addClickEvent('keyEqualToId', function() {view.setFinalExpression('screenId') });
+        this.addClickEvent('keyClearId', function() {view.clearScreen('screenId') });
+        this.addClickEvent('keyBackspaceId', function() {view.setInnerHTMLBackspace('screenId') });
+    },
 
-	setInnerHTML: function (id, innerHTML) {
-		document.getElementById(id).innerHTML = this.getInnerHTML(id) + innerHTML;
+    setInnerHTML: function (id, innerHTML) {
+        document.getElementById(id).innerHTML = this.getInnerHTML(id) + innerHTML;
     },
 
     setInnerHTMLOneByX: function(id) {
-    	document.getElementById(id).innerHTML = '1/(' + this.getInnerHTML(id) + ')';
+	    document.getElementById(id).innerHTML = '1/(' + this.getInnerHTML(id) + ')';
     },
 
     setInnerHTMLUnary: function(id) {
-    	document.getElementById(id).innerHTML = '-(' + this.getInnerHTML(id) + ')';
+   	    document.getElementById(id).innerHTML = '-(' + this.getInnerHTML(id) + ')';
     },
 
     setInnerHTMLBackspace: function(id) {
-    	var expr = this.getInnerHTML(id);
-    	expr = expr.substring(0, expr.length - 1);
-    	document.getElementById(id).innerHTML = expr;
+        var expr = this.getInnerHTML(id);
+        expr = expr.substring(0, expr.length - 1);
+        document.getElementById(id).innerHTML = expr;
     },
 
     setFinalExpression: function(id) {
@@ -281,7 +272,6 @@ window.view = {
         /*catch (e) {
             document.getElementById(id).innerHTML = "Undefined";
         }*/
-
     },
 
     getInnerHTML: function (id) {
@@ -290,16 +280,14 @@ window.view = {
     },
 
     clearScreen: function (id) {
-    	document.getElementById(id).innerHTML = '';
+        document.getElementById(id).innerHTML = '';
     },
 
     init: function () {
         this.activateEvents();
     }
-
-
 }
 
 window.onload = function () {
-    window.view.init();
+  window.view.init();
 }
